bd17225771e2ff38e258cff1d596253c85bca107
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-10-29 15:55:40 -0500

    user model added, database sync setup:

---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+const userSchema = (sequelizeDatabase, DataTypes) => {
+    const model = sequelizeDatabase.define('user', {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+
+    model.beforeCreate(async (user) => {
+        let hashedPassword = await bcrypt.hash(user.password, 5);
+        console.log('Hashed password in beforeCreate: ', bcrypt.hashedPassword);
+        user.password = hashedPassword;
+    });
+
+    return model;
+};
+
+module.exports = userSchema;
